0c07015b7f6afa65eef2c8debaed9b5fe93f9b70
com-progresso <comprogresso@outlook.com>
2017-03-06 21:13:04 -0300

    Add files via upload

---
+++ js/kenburn.js
@@ -0,0 +1,10 @@
+$(".slideshow-bg main.background").vegas({
+	delay: 9000,
+    slides: [
+        { src: 'img/kbg-1.jpg' },
+        { src: 'img/kbg-2.jpg' },
+        { src: 'img/kbg-3.jpg' },
+        { src: 'img/kbg-4.jpg' }
+    ],
+    animation: 'kenburns'
+});
